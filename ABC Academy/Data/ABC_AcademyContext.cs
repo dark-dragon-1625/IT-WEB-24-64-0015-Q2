56e6b3c86fb7f5892138ed686bc8afccb6c22463
dark-dragon-1625 <sachinthamadusanka1602@gmail.com>
2024-10-28 02:55:38 +0530

    Add files via upload

---
+++ ABC Academy/Data/ABC_AcademyContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ABC_Academy.Model;
+
+namespace ABC_Academy.Data
+{
+    public class ABC_AcademyContext : DbContext
+    {
+        public ABC_AcademyContext (DbContextOptions<ABC_AcademyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ABC_Academy.Model.Course> Course { get; set; } = default!;
+        public DbSet<ABC_Academy.Model.Student> Student { get; set; } = default!;
+    }
+}
